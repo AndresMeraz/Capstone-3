470756ba657ef840b12459e499e9c59aa9982b23
AndresMeraz <andres.meraz312@gmail.com>
2023-12-20 16:06:16 -0800

    started functionality

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,30 @@
 'use strict'
+window.onload = init;
 
-alert('reg')
+function init(){
+    const regForm = document.getElementById("registrationForm");
+    regForm.addEventListener("submit", registerNewUser);
+}
+
+async function registerNewUser(event) {
+    event.preventDefault();
+    class user{
+        constructor(username, fullName, password){
+        this.username = document.getElementById("username").value;
+        this.fullName = document.getElementById("fullName").value;
+        this.password = document.getElementById("password").value;
+        }
+    }
+    try {
+        const response = await fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
+            method:'POST',
+            body: JSON.stringify(user)
+            })
+        const data = await response.json();  
+        console.log(`User ${user} added succesfully`)
+    }
+        catch (error) {
+            console.error('Error', error)
+        }
+        // window.location.href = "index.html"
+    }
